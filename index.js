c672abc325e61c28cdc913a40068f2efd3b4afba
John Hunter Allen <hallen36@gmail.com>
2021-12-12 11:22:14 -0500

    commit tree ba68af31a52b165711d243742ff58344ef7b9ab0

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
-
 // Write your code here
+let multiply
+let num1 = 31
+let num2 = 2
+multiply = num1*num2;
+
+let random
+random = Number.isInteger(Math.random() * 10) + 1;
+
+let mod
+let num3 = 9
+let num4 = 5
+mod = num3%num4;
+
+let max
+max = Math.max(1, 2, 3, 4, 20);
